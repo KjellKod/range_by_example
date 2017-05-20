4c6883cf3d784949218de54df5ea01d70c272da9
Kjell Hedstrom <kjell.hedstrom@logrhythm.com>
2017-05-19 23:01:14 -0600

    test from docker

--- scripts/travis/ubuntu-16-build.sh
+++ scripts/travis/ubuntu-16-build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+sudo uname -a
+sudo lsb_release -a
+sudo add-apt-repository -y ppa:jonathonf/gcc-7.1
+sudo apt-get update
+sudo apt-get install gcc-7
+sudo apt-get install g++-7
+
 git clone https://github.com/ericniebler/range-v3.git
 cp -r range-v3/include/* /usr/local/include/.
 
